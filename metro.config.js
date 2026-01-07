9b1ce5e144e5cd2d7c70e44b8f74cbc8f50de2d5
psychominity <psychominity@gmail.com>
2026-01-08 00:21:33 +0900

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,17 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Add blockList to exclude problematic cache files
+config.resolver = {
+  ...config.resolver,
+  blockList: [
+    /node_modules\/react-native-css-interop\/\.cache\/.*/,
+  ],
+};
+
+// Ensure watchFolders includes the project root
+config.watchFolders = [__dirname];
+
 module.exports = withNativeWind(config, {
   input: "./global.css",
   // Force write CSS to file system instead of virtual modules
